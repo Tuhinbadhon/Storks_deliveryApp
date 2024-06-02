95e0c498614cb08954dfbfd6455373eb276b9c04
Md.Fazlul Haque <94878259+Tuhinbadhon@users.noreply.github.com>
2024-06-02 18:36:57 +0600

    footer update

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -7,8 +7,8 @@ const Footer = () => {
     <div className=" ">
       {/* className="" */}
       <footer
-        className="  to-transparent dark:bg-indigo-50 dark:text-black md:py-20 py-7
-      bg-opacity-5  place-items-center w-full gap-4 
+        className="  bg-base-300    md:py-28 py-7
+        place-items-center w-full gap-4 
             text-base-content flex flex-col   md:px-14  lg:mt-20 mt-5 px-5 "
       >
         <div className="w-full flex  lg:flex-row md:flex-row md:justify-between flex-col gap-4 lg:justify-between ">
@@ -16,34 +16,30 @@ const Footer = () => {
             <img className="w-20 " src="/logo.png" alt="" />
             <Link to="/" className=" ">
               <div className=" flex flex-col  lg:text-start md:items-start items-center p-0 ml-0  ">
-                <h2 className="uppercase text-2xl max-[450px]:text-xl  text-black   font-extrabold ">
+                <h2 className="uppercase text-2xl max-[450px]:text-xl     font-extrabold ">
                   storks
                 </h2>
-                <p className="uppercase text-xs  text-black  font-bold  ">
+                <p className="uppercase text-xs     font-bold  ">
                   We deliver parcel worldwide{" "}
                 </p>
               </div>
             </Link>
           </div>
           <div className="flex flex-col  text-center  ">
-            <h6 className="font-bold text-black md:mb-5 ">USEFUL LINKS</h6>
-            <a className="link link-hover text-gray-700">History</a>
-            <a className="link link-hover text-gray-700">Services</a>
-            <a className="link link-hover text-gray-700">About Us</a>
-            <a className="link link-hover text-gray-700">Company Timeline</a>
+            <h6 className="font-bold   md:mb-5 ">USEFUL LINKS</h6>
+            <a className="link link-hover ">History</a>
+            <a className="link link-hover   ">Services</a>
+            <a className="link link-hover   ">About Us</a>
+            <a className="link link-hover   ">Company Timeline</a>
           </div>
           <div className="flex flex-col gap-1 text-center">
-            <h6 className="font-bold text-black md:mb-5">Contacts</h6>
-            <a className="link link-hover text-gray-700">+8801815-0000000</a>
-            <a className="link link-hover text-gray-700">
-              tuhinbadhon@gmail.com
-            </a>
-            <a className="link link-hover text-gray-700">
-              Address: Dhanmondi,Dhaka
-            </a>
+            <h6 className="font-bold   md:mb-5">Contacts</h6>
+            <a className="link link-hover   ">+8801815-0000000</a>
+            <a className="link link-hover   ">tuhinbadhon@gmail.com</a>
+            <a className="link link-hover   ">Address: Dhanmondi,Dhaka</a>
           </div>
           <div className="text-center">
-            <h6 className="font-bold text-black md:mb-5 ">Social</h6>
+            <h6 className="font-bold   md:mb-5 ">Social</h6>
             <div className="grid grid-flow-col gap-4 justify-center">
               <a href="">
                 <FaFacebook className="text-4xl text-blue-600" />
@@ -58,12 +54,10 @@ const Footer = () => {
           </div>
           <div>
             <form className=" flex flex-col items-center  ">
-              <h6 className="font-bold text-black md:mb-5">Newsletter</h6>
+              <h6 className="font-bold   md:mb-5">Newsletter</h6>
               <fieldset className="form-control w-80">
                 <label className="label">
-                  <span className="text-gray-700">
-                    Enter your email address
-                  </span>
+                  <span className="  ">Enter your email address</span>
                 </label>
                 <div className="join">
                   <input
--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -5,11 +5,12 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
+import "swiper/css/effect-fade";
 
 import "./Banner.css";
 
 // import required modules
-import { Pagination, Navigation } from "swiper/modules";
+import { Pagination, Navigation, EffectFade } from "swiper/modules";
 
 import img1 from "../../assets/banner1.jpg";
 import img2 from "../../assets/banner2.jpg";
@@ -18,32 +19,116 @@ import img4 from "../../assets/banner4.jpg";
 
 const Banner = () => {
   return (
-    <div className=" overflow-auto">
-      <Swiper
-        slidesPerView={1}
-        spaceBetween={30}
-        loop={true}
-        pagination={{
-          clickable: true,
-        }}
-        navigation={true}
-        modules={[Pagination, Navigation]}
-        className="mySwiper lg:max-w-full  md:max-w-screen-md sm:max-w-screen-sm  
+    <div>
+      <div className="  overflow-auto">
+        <Swiper
+          speed={1500}
+          effect={"fade"}
+          slidesPerView={1}
+          spaceBetween={30}
+          loop={true}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          modules={[Pagination, EffectFade, Navigation]}
+          className="mySwiper lg:max-w-full  md:max-w-screen-md sm:max-w-screen-sm  
             "
-      >
-        <SwiperSlide>
-          <img src={img1} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={img2} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={img3} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={img4} />
-        </SwiperSlide>
-      </Swiper>
+        >
+          <SwiperSlide className="relative">
+            <img src={img1} />
+            <div className="absolute   h-full flex left-0 top-0 items-center justify-center bg-gradient-to-r from-gray-800  to-[rgba(21,21,21,0)] max-[450px]:hidden">
+              <div className="space-y-7 w-1/2 text-white text-center ">
+                <h2 className="font-bold lg:text-5xl md:text-2xl ">
+                  Your Child is Our Asset
+                </h2>
+                <p>
+                  We have 20 years Experience on this profession.it is pleasure
+                  but because those who do not know how to pursue pleasure
+                  rationally encounter consequences that are extremley painfull{" "}
+                </p>
+                <div className="">
+                  <button className="btn btn-primary mr-5">
+                    CONTACT US TODAY
+                  </button>
+                  <button className="btn btn-outline btn-secondary">
+                    ENROL NOW
+                  </button>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className="relative">
+            <img src={img2} />
+            <div className="absolute   h-full flex left-0 top-0 items-center justify-center bg-gradient-to-r from-gray-800 to-[rgba(21,21,21,0)] max-[450px]:hidden">
+              <div className="space-y-7 w-1/2 text-white text-center ">
+                <h2 className="font-bold lg:text-5xl md:text-2xl ">
+                  Your Child is Our Asset
+                </h2>
+                <p>
+                  We have 20 years Experience on this profession.it is pleasure
+                  but because those who do not know how to pursue pleasure
+                  rationally encounter consequences that are extremley painfull{" "}
+                </p>
+                <div className="">
+                  <button className="btn btn-primary mr-5">
+                    CONTACT US TODAY
+                  </button>
+                  <button className="btn btn-outline btn-secondary">
+                    ENROL NOW
+                  </button>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className="relative">
+            <img src={img3} />
+            <div className="absolute   h-full flex left-0 top-0 items-center justify-center bg-gradient-to-r from-gray-800 to-[rgba(21,21,21,0)] max-[450px]:hidden">
+              <div className="space-y-7 w-1/2 text-white text-center ">
+                <h2 className="font-bold lg:text-5xl md:text-2xl ">
+                  Your Child is Our Asset
+                </h2>
+                <p>
+                  We have 20 years Experience on this profession.it is pleasure
+                  but because those who do not know how to pursue pleasure
+                  rationally encounter consequences that are extremley painfull{" "}
+                </p>
+                <div className="">
+                  <button className="btn btn-primary mr-5">
+                    CONTACT US TODAY
+                  </button>
+                  <button className="btn btn-outline btn-secondary">
+                    ENROL NOW
+                  </button>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className="relative">
+            <img src={img4} />
+            <div className="absolute   h-full flex left-0 top-0 items-center justify-center bg-gradient-to-r from-gray-800 to-[rgba(21,21,21,0)] max-[450px]:hidden">
+              <div className="space-y-7 w-1/2 text-white text-center ">
+                <h2 className="font-bold lg:text-5xl md:text-2xl ">
+                  Your Child is Our Asset
+                </h2>
+                <p>
+                  We have 20 years Experience on this profession.it is pleasure
+                  but because those who do not know how to pursue pleasure
+                  rationally encounter consequences that are extremley painfull{" "}
+                </p>
+                <div className="">
+                  <button className="btn btn-primary mr-5">
+                    CONTACT US TODAY
+                  </button>
+                  <button className="btn btn-outline btn-secondary">
+                    ENROL NOW
+                  </button>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+        </Swiper>
+      </div>
     </div>
   );
 };
