afff51f1a663d0703e08341f3fc4dc58bd6f619c
Tuhinbadhon <tuhinbadhon@gmail.com>
2024-09-01 19:26:33 +0600

    nav updated

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -78,7 +78,7 @@ const Navbar = () => {
           <li>
             <Link
               to="/login"
-              className="hover:border-b-2 hover:bg-opacity-20 hover:border-blue-700 max-[450px]:mt-2 md:hidden"
+              className="hover:border-b-2 hover:bg-opacity-20 hover:border-blue-700 max-[450px]:mt-2 lg:hidden "
               onClick={() => setIsDropdownOpen(false)}
             >
               Login / Sign Up
@@ -155,10 +155,10 @@ const Navbar = () => {
       <div className="navbar py-3 fixed z-10 bg-opacity-60 min-w-screen-xl mx-auto text-white bg-black">
         <div className="navbar-start">
           <button
-            className="lg:hidden p-4 focus:outline-none"
+            className="lg:hidden  mr-2  border border-gray-600 p-[6px] rounded-md focus:outline-none"
             onClick={toggleSidebar}
           >
-            <LuAlignJustify className="text-xl" />
+            <LuAlignJustify size={14} className="text-[#ccd0d9]" />
           </button>
 
           {/* Sidebar for small screens */}
@@ -202,7 +202,7 @@ const Navbar = () => {
             </Link>
             <Link to="/" className="">
               <div className="flex flex-col text-start items-start p-0 ml-0">
-                <h2 className="uppercase text-2xl max-[450px]:text-base md:text-base font-extrabold">
+                <h2 className="uppercase text-2xl max-[450px]:text-lg md:text-base font-extrabold">
                   storks
                 </h2>
                 <p className=" max-[450px]:hidden uppercase text-xs md:text-[10px] font-bold">
